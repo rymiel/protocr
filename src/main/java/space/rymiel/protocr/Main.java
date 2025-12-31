cfee3faf4cdc25c00b6d32c10008adc94c57b211
Emilia Kond <emilia@rymiel.space>
2025-12-31 05:03:24 +0200

    Mark as supporting editions

--- src/main/java/space/rymiel/protocr/Main.java
+++ src/main/java/space/rymiel/protocr/Main.java
@@ -5,11 +5,17 @@ import com.google.protobuf.compiler.PluginProtos;
 
 import java.io.IOException;
 
-import static com.google.protobuf.compiler.PluginProtos.CodeGeneratorResponse.Feature.FEATURE_PROTO3_OPTIONAL;
+import static com.google.protobuf.DescriptorProtos.Edition.EDITION_2023_VALUE;
+import static com.google.protobuf.DescriptorProtos.Edition.EDITION_2024_VALUE;
+import static com.google.protobuf.compiler.PluginProtos.CodeGeneratorResponse.Feature.FEATURE_PROTO3_OPTIONAL_VALUE;
+import static com.google.protobuf.compiler.PluginProtos.CodeGeneratorResponse.Feature.FEATURE_SUPPORTS_EDITIONS_VALUE;
 
 public class Main {
   public static void main(String[] args) throws IOException {
-    var results = PluginProtos.CodeGeneratorResponse.newBuilder().setSupportedFeatures(FEATURE_PROTO3_OPTIONAL.getNumber());
+    var results = PluginProtos.CodeGeneratorResponse.newBuilder()
+        .setSupportedFeatures(FEATURE_PROTO3_OPTIONAL_VALUE | FEATURE_SUPPORTS_EDITIONS_VALUE)
+        .setMinimumEdition(EDITION_2023_VALUE)
+        .setMaximumEdition(EDITION_2024_VALUE);
     var thing = PluginProtos.CodeGeneratorRequest.parseFrom(System.in);
 
     for (var filename : thing.getFileToGenerateList()) {
